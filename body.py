1df7de7d0d19a2345dd7f3d2f9cbb69834bb933e
Dima <pikuza142@gmail.com>
2023-06-14 14:06:52 +0300

    gwh

--- body.py
+++ body.py
@@ -1,21 +1,69 @@
 from aiogram import Bot, Dispatcher, types, executor
 
-
 from env import TOKEN
 
-from User_class import *
+import aioschedule 
+from apscheduler.schedulers.background import BackgroundScheduler
+
+import asyncio
 
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
+
 import datetime
+
+from send_words import make, check
+
+today_result = [0, 0, 0, 0, 0]
+
+mistake_words = []
+
+last_word = 'back off - отстраниться, пойти на попятный, отступить'
 
 COMAND = """
 /start
 /get_score
 /change_time
 """
+
+scheduler = BackgroundScheduler()
+
 bot = Bot(token= TOKEN)
 dp = Dispatcher(bot, storage = MemoryStorage())
+scheduler.add_job(make, trigger = "interval", args = [bot, ], )
+
+async def scheduler_morning(state: FSMContext):
+    data = state.get_data()
+    if datetime.datetime.now() > data["time_on"]:
+        global today_result
+        list = aioschedule.every().day.do(make, last_word, mistake_words)
+        state.update_data({'today_words':list})
+        last_word = list[4]
+        today_result = [0, 0, 0, 0, 0]
+
+async def scheduler_night(state: FSMContext):
+    data = state.get_data()
+    h , m = map(int, data["time_out"].split(":"))
+    if data["time_out"]>15:
+        if datetime.datetime.now().time().hour >= h and datetime.datetime.now().time().minute >= m:
+            data = state.get_data()
+            for i in range(5):
+                if today_result[i] < 3:
+                    mistake_words += data["today_words"]
+    else:
+        if datetime.datetime.now().time().hour >= h and datetime.datetime.now().time().minute >= m and datetime.datetime.now().time()<15:
+            data = state.get_data()
+            for i in range(5):
+                if today_result[i] < 3:
+                    mistake_words += data["today_words"]
+
+    
+async def scheduler(state: FSMContext):
+    data = state.get_data()
+    if datetime.datetime.now() > data["time_out"]:
+        list = aioschedule.every().day.do(make, bot, last_word)
+        state.update_data({'today_words':list})
+        last_word = list[4]
 
 @dp.message_handler(commands= ["help"], state= '*')
 async def echo(message: types.Message):
@@ -43,7 +91,6 @@ async def echo(message: types.Message, state: FSMContext):
 async def set_age(message: types.Message, state: FSMContext):
     time_out = message.text
     try:
-        time_out = datetime.datetime.strptime(time_out, "%H:%M").time()
         await state.update_data({"night_time": time_out})
         await message.answer(text = "Теперь напиши время, когда ты просыпаешся(например 7:00)")
         await state.set_state("q2")
@@ -55,7 +102,6 @@ async def set_age(message: types.Message, state: FSMContext):
     time_on = str(message.text)
     data = await state.get_data()
     try:
-        time_on = datetime.datetime.strptime(time_on, "%H:%M").time()
         await state.update_data({"morning_time": time_on})
         await message.answer(text = "Всё, ты закончил регистрацию")
         await state.set_state("q3")
@@ -65,5 +111,28 @@ async def set_age(message: types.Message, state: FSMContext):
         await message.answer(text = "Ты не правильно ввёл время(<i><b>ПРИМЕР</b></i> 22:00)", parse_mode = "HTML")
     print(await state.get_data())
 
+@dp.message_handler(state = 'q3')
+async def echo(message: types.Message, state: FSMContext):
+    text = message.text.split('\n')
+    if text[0] == '':
+        global today_result
+        data = await state.get_data()
+        score = data["score"]
+        result = check()
+        for i in range(5):
+            res = result[i]
+            if i:
+                score+=1
+                today_result[i]+1
+            else:
+                score -= 1
+
+
+
+
+
+
+
 if __name__ == "__main__":
+    scheduler.start()
     executor.start_polling(dp, skip_updates=True)
--- send_words.py
+++ send_words.py
@@ -1,14 +1,40 @@
-from aiogram import types
+from string import ascii_letters
 
-async def send_words(message: types.Message, last_word_number, mistake_words = []):
+async def make(last_word, mistake_words = []):
     sending_words = mistake_words
     with open('lisst_of_words.txt') as file:
         file = file.readlines()
         f = [i for i in file.split()]
-    index = await f.index(last_word_number)+1
+    
+    index = await f.index(last_word)+1
     while len(sending_words)!= 5 or index != len(f):
         sending_words += [f[index]]
         index+=1
-    await message.answer()
+        sending_words = sending_words.join("\n")
+        sending_words.replace("-------", " - ")
+    return sending_words
 
-#async def 
+def check(answers, today_words):
+    result = []
+    with open('lisst_of_words.txt') as file:
+        file = file.readlines()
+        file = [i for i in file.split()]
+        for i in range(5):
+            word = today_words[i]
+            answer = answer[i]
+            index = file.index(word)
+            liter = ascii_letters()
+            if word[0] in liter:
+                if set(file[index].split("-------")[0]) == set([answer]):
+                    result +=[1]
+                else:
+                    result += [0]
+            else:
+                if set(file[index].split("-------")[1].split()) == set([answer]):
+                    result +=[1]
+                else:
+                    result += [0]
+
+
+
+    return result
